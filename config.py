3042edbdf57ebd90c55a71d758409dfc7035ef2f
Thomas Briot <thomas.briot82@gmail.com>
2018-08-18 07:19:00 -0400

    Removed config.py from .gitignore

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 # SSH Tunnel config
-REMOTE_SERVER_HOST = "ec2-123-456-789.ca-central-1.compute.amazonaws.com" # AWS EC2 instance = "jumpbox"
+REMOTE_SERVER_HOST = "ec2-35-183-33-109.ca-central-1.compute.amazonaws.com" # AWS EC2 instance = "jumpbox"
 REMOTE_SERVER_USERNAME = 'ec2-user'
-REMOTE_SERVER_PRIVATE_KEY = r"C:\Users\jdoe\.ssh\jdoe-ca-central-1.pem"
+REMOTE_SERVER_PRIVATE_KEY = r"C:\Users\Thomas\.ssh\tbriot-ca-central-1.pem"
 
-PRIVATE_SERVER_HOST = "jdoe-db.ca-central-1.rds.amazonaws.com" # MySQL db in private AWS subnet
+PRIVATE_SERVER_HOST = "investornetwork.co0pqf5yoscl.ca-central-1.rds.amazonaws.com" # MySQL db in private AWS subnet
 PRIVATE_SERVER_PORT = 3306 # MySQL db port
 
 LOCAL_HOST = '127.0.0.1'
